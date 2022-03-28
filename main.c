76c27b04eea2642a15561a7ba9dce9ffdfa57e05
Matheus Augusto <matheus-augusto.santos@unesp.br>
2022-03-28 13:40:15 -0300

    First application of parser rules

--- main.c
+++ main.c
@@ -4,10 +4,36 @@
 
 #include <editline/history.h>
 
-#define VERSION_INFO "0.0.1"
+#include "mpc/mpc.h"
+
+#define VERSION_INFO "0.0.2"
+
+typedef struct
+{
+	const char* name;
+	const char* definition;
+	mpc_parser_t* parser_ptr;
+} Parser_t;
 
 int main()
 {
+	const char* parser_names[] = {
+		"number", "operator", "expr", "misp"
+	};
+
+	mpc_parser_t* Number, * Operator, * Expr, * Misp;
+	mpc_parser_t** parsers[4] = { &Number, &Operator, &Expr, &Misp };
+
+	for (int i = 0; i < sizeof parsers / sizeof *parsers; ++i)
+		*parsers[i] = mpc_new(parser_names[i]);
+
+	mpca_lang(MPCA_LANG_DEFAULT, ""
+								 "number 	: /-?[0-9]+/ ;"
+								 "operator	: '+' | '-' | '*' | '/' | '%' ;"
+								 "expr		: <number> | '(' <operator> <expr>+ ')' ;"
+								 "misp		: /^/ <operator> <expr>+ /$/ ;",
+		Number, Operator, Expr, Misp);
+
 	puts("Misp Version " VERSION_INFO);
 	puts("Press Ctrl+C to exit\n");
 
@@ -18,11 +44,22 @@ int main()
 			char* input = readline("Misp>>> ");
 			add_history(input);
 
-			printf("> %s\n", input);
+			mpc_result_t r;
+			if (mpc_parse("<stdin>", input, Misp, &r))
+			{
+				mpc_ast_print(r.output);
+				mpc_ast_delete(r.output);
+			}
+			else
+			{
+				mpc_err_print(r.error);
+				mpc_err_print(r.error);
+			}
 
 			free(input);
 		}
 	}
 
+	mpc_cleanup(4, Number, Operator, Expr, Misp);
 	return 0;
 }
