24fbc54f3d69250c9c98dbe1533cc6d5ba09fcc7
Matheus Augusto <matheus-augusto.santos@unesp.br>
2022-03-29 18:06:54 -0300

    first implementation

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #include <editline/history.h>
+#include <stdbool.h>
+#include <stdint.h>
 
 #include "mpc/mpc.h"
 
@@ -28,30 +30,91 @@ typedef enum merr_type
 	MERR_BAD_TYPE,
 } error_t;
 
-typedef long num_t;
-typedef long double flt_t;
+typedef struct MispValue mval_t;
 
-typedef struct MispValue
+struct MispValue
 {
 	enum mval_type type;
 
 	union
 	{
-		num_t num;
-		flt_t flt;
-		error_t err;
+		struct MispList
+		{
+			int count;
+			mval_t** cells;
+		} list;
+
+		error_t _err;
+		char* err;
+
+		char* sym;
+		long num;
+		long double flt;
 	};
+};
 
-} mval_t;
+static const struct MispList MispEmptyList = {
+	.count = 0, .cells = NULL,
+};
 
-mval_t mval_num(long x)
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "bugprone-macro-parentheses"
+#define MVAL_CTOR(name, arg, type, field_set) \
+    mval_t* mval_ ## name (arg) {             \
+        mval_t* v = malloc(sizeof *v);        \
+        *v = (mval_t) {(type), field_set,};   \
+        return v;                             \
+    }
+#pragma clang diagnostic pop
+
+MVAL_CTOR(numbr, long x, MVAL_NUMBR, .num = x)
+MVAL_CTOR(error, char* m, MVAL_ERROR, .err = strdup(m))
+MVAL_CTOR(float, long double x, MVAL_FLOAT, .flt = x)
+MVAL_CTOR(symbl, char* s, MVAL_SYMBL, .sym = strdup(s))
+MVAL_CTOR(sexpr, void, MVAL_SEXPR, .list = MispEmptyList)
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "misc-no-recursion"
+void mval_delete(mval_t* v)
+{
+	switch (v->type)
+	{
+	case MVAL_SEXPR:
+	{
+		for_range(i, 0, v->list.count)
+		{
+			mval_delete(v->list.cells[i]);
+		}
+
+		free(v->list.cells);
+		break;
+	}
+
+	case MVAL_ERROR:
+		free(v->err);
+		break;
+
+	case MVAL_SYMBL:
+		free(v->sym);
+		break;
+
+	case MVAL_NUMBR:
+	case MVAL_FLOAT:
+		break;
+	}
+
+	free(v);
+}
+#pragma clang diagnostic pop
+
+mval_t $mval_num(long x)
 {
 	return (mval_t){ MVAL_NUMBR, .num = x, };
 }
 
-mval_t mval_err(error_t x)
+mval_t $mval_err(error_t x)
 {
-	return (mval_t){ MVAL_ERROR, .err = x, };
+	return (mval_t){ MVAL_ERROR, ._err = x, };
 }
 
 mval_t mval_flt(long double x)
@@ -59,7 +122,49 @@ mval_t mval_flt(long double x)
 	return (mval_t){ MVAL_FLOAT, .flt = x, };
 }
 
-void mval_print(mval_t v)
+void mval_print(mval_t* v);
+void mval_sexpr_print(struct MispList* v);
+
+void mval_print(mval_t* v)
+{
+	switch (v->type)
+	{
+	case MVAL_SEXPR:
+		mval_sexpr_print(&v->list);
+		break;
+	case MVAL_ERROR:
+		printf("Error: %s", v->err);
+		break;
+	case MVAL_SYMBL:
+		printf("%s", v->sym);
+		break;
+	case MVAL_NUMBR:
+		printf("%li", v->num);
+		break;
+	case MVAL_FLOAT:
+		printf("%Lf", v->flt);
+		break;
+	}
+}
+
+void mval_sexpr_print(struct MispList* l)
+{
+	putchar('(');
+
+	for_range(i, 0, l->count)
+	{
+		mval_print(l->cells[i]);
+
+		if (i != (l->count - 1))
+		{
+			putchar(' ');
+		}
+	}
+
+	putchar(')');
+}
+
+void $mval_print(mval_t v)
 {
 	switch (v.type)
 	{
@@ -67,23 +172,79 @@ void mval_print(mval_t v)
 		printf("%li", v.num);
 		return;
 	case MVAL_ERROR:
-		printf("Error: %s", (const char* []){
-			"Division by Zero",
-			"Invalid Operator",
-			"Invalid Number",
-			"Unexpected Type",
-		}[v.err]);
+		printf("Error: %s", v.err);
 		return;
 	case MVAL_FLOAT:
 		printf("%Lf", v.flt);
 	}
 }
 
-void mval_println(mval_t v)
+void mval_println(mval_t* v)
 {
 	mval_print(v);
 	putchar('\n');
 }
+
+mval_t* mval_read_num(const char* s)
+{
+	errno = 0;
+	long double value = strtold(s, NULL);
+
+	if (errno == ERANGE)
+		return mval_error("Invalid Number");
+
+	switch ((intptr_t)strchr(s, '.'))
+	{
+	case 0: // Integer
+		return mval_numbr((long)value);
+	default:
+		return mval_float(value);
+	}
+}
+
+void mval_list_add(struct MispList* v, mval_t* x)
+{
+	v->count++;
+	v->cells = realloc(v->cells, sizeof(mval_t*) * v->count);
+	v->cells[v->count - 1] = x;
+}
+
+bool hasMispExpression(mpc_ast_t* const* child)
+{
+	return !(strcmp((*child)->tag, "regex") == 0
+			 || strcmp((*child)->contents, "(") == 0
+			 || strcmp((*child)->contents, ")") == 0);
+}
+
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "misc-no-recursion"
+mval_t* mval_read(mpc_ast_t* t)
+{
+	const char* tag = t->tag;
+	if (strstr(tag, "number"))
+		return mval_read_num(t->contents);
+	if (strstr(tag, "symbol"))
+		return mval_symbl(t->contents);
+
+	// Is not root and is not sexpr
+	if (strcmp(tag, ">") != 0 && !strstr(tag, "sexpr"))
+		return mval_error("Invalid token");
+
+	mval_t* x = mval_sexpr();
+
+	for (mpc_ast_t** child = t->children;
+		 child != t->children + t->children_num;
+		 ++child)
+	{
+		if (hasMispExpression(child))
+		{
+			mval_list_add(&x->list, mval_read(*child));
+		}
+	}
+
+	return x;
+}
+#pragma clang diagnostic pop
 
 mpc_parser_t** create_language(void)
 {
@@ -106,7 +267,7 @@ mpc_parser_t** create_language(void)
 	static mpc_parser_t* parsers[len(parser_properties)];
 	static char language_grammar[2048];
 
-	for_range(i, 0, 4)
+	for_range(i, 0, len(parser_properties))
 	{
 		parsers[i] = mpc_new(parser_properties[i].name);
 
@@ -142,12 +303,12 @@ int main()
 
 	while (1)
 	{
-		char* input = readline("Misp> ");
+		char* const input = readline("Misp> ");
 
 		if (strlen(input) == 0)
 		{
 			free(input);
-			break;
+			return 0;
 		}
 
 		add_history(input);
@@ -155,8 +316,9 @@ int main()
 		mpc_result_t r;
 		if (mpc_parse("<stdin>", input, parsers[4], &r))
 		{
-			mval_t result = evaluate_tree(r.output);
-			mval_println(result);
+			mval_t* v = mval_read(r.output);
+			mval_println(v);
+			mval_delete(v);
 
 			mpc_ast_delete(r.output);
 		}
@@ -168,8 +330,6 @@ int main()
 
 		free(input);
 	}
-
-	return 0;
 }
 
 enum op_type
@@ -226,16 +386,16 @@ mval_t eval_op_flt(mval_t x_, char* op, mval_t y_)
 		return mval_flt(x * y);
 	case DIV:
 		return y == 0.0
-			   ? mval_err(MERR_DIV_ZERO)
+			   ? $mval_err(MERR_DIV_ZERO)
 			   : mval_flt(x / y);
 	case MOD:
-		return mval_err(MERR_BAD_TYPE);
+		return $mval_err(MERR_BAD_TYPE);
 	case MIN:
 		return mval_flt(x <= y ? x : y);
 	case MAX:
 		return mval_flt(x >= y ? x : y);
 	default:
-		return mval_err(MERR_BAD_OPER);
+		return $mval_err(MERR_BAD_OPER);
 	}
 }
 
@@ -246,27 +406,27 @@ mval_t eval_op_num(mval_t x_, char* op, mval_t y_)
 	switch (operator_type(op))
 	{
 	case EXP:
-		return mval_num((long)powl(x, y));
+		return $mval_num((long)powl(x, y));
 	case ADD:
-		return mval_num(x + y);
+		return $mval_num(x + y);
 	case SUB:
-		return mval_num(x - y);
+		return $mval_num(x - y);
 	case MUL:
-		return mval_num(x * y);
+		return $mval_num(x * y);
 	case DIV:
 		return y == 0
-			   ? mval_err(MERR_DIV_ZERO)
-			   : mval_num(x / y);
+			   ? $mval_err(MERR_DIV_ZERO)
+			   : $mval_num(x / y);
 	case MOD:
 		return y == 0
-			   ? mval_err(MERR_DIV_ZERO)
-			   : mval_num(x % y);
+			   ? $mval_err(MERR_DIV_ZERO)
+			   : $mval_num(x % y);
 	case MIN:
-		return mval_num(x <= y ? x : y);
+		return $mval_num(x <= y ? x : y);
 	case MAX:
-		return mval_num(x >= y ? x : y);
+		return $mval_num(x >= y ? x : y);
 	default:
-		return mval_err(MERR_BAD_OPER);
+		return $mval_err(MERR_BAD_OPER);
 	}
 }
 
@@ -295,11 +455,11 @@ mval_t evaluate_tree(mpc_ast_t* t)
 		long double value = strtold(t->contents, NULL);
 
 		if (errno == ERANGE)
-			return mval_err(MERR_BAD_NUM);
+			return $mval_err(MERR_BAD_NUM);
 
 		if (strchr(t->contents, '.'))
 			return mval_flt(value);
-		return mval_num((long)value);
+		return $mval_num((long)value);
 	}
 
 	char* op = t->children[1]->contents;
@@ -313,7 +473,7 @@ mval_t evaluate_tree(mpc_ast_t* t)
 		case MVAL_ERROR:
 			return x;
 		case MVAL_NUMBR:
-			return mval_num(-x.num);
+			return $mval_num(-x.num);
 		case MVAL_FLOAT:
 			return mval_flt(-x.flt);
 		}
