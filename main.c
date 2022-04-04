2ba2315ac938f379ab0e1073da4fe441896043ae
Matheus Augusto <matheus-augusto.santos@unesp.br>
2022-04-03 22:08:39 -0300

    Refactor main

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include <editline/history.h>
 
@@ -9,38 +10,57 @@
 
 #define VERSION_INFO "0.0.14"
 
-int interactive_prompt(menv* e)
+static menv* GLOBAL_ENV = NULL;
+
+void init_global_env()
+{
+	GLOBAL_ENV = menv_new();
+	menv_add_builtins(GLOBAL_ENV);
+}
+
+void delete_global_env(void)
+{
+	assert(GLOBAL_ENV != NULL);
+	menv_del(GLOBAL_ENV);
+}
+
+void interpret_input(menv* e, const char* input)
+{
+	mpc_result_t r;
+	if (mpc_parse("<stdin>", input, get_lang_parser(), &r))
+	{
+		mval* v = mval_read(r.output);
+		v = mval_eval(e, v);
+		mval_println(v);
+		mval_del(v);
+
+		mpc_ast_delete(r.output);
+	}
+	else
+	{
+		mpc_err_print(r.error);
+		mpc_err_delete(r.error);
+	}
+}
+
+void free_input(char* const* s)
+{
+	free(*s);
+}
+
+int interactive_prompt(void)
 {
 	while (1)
 	{
+		__attribute__((__cleanup__(free_input)))
 		char* const input = readline("misp> ");
 
 		if (strlen(input) == 0)
-		{
-			free(input);
-			menv_del(e);
 			return 0;
-		}
 
 		add_history(input);
 
-		mpc_result_t r;
-		if (mpc_parse("<stdin>", input, get_lang_parser(), &r))
-		{
-			mval* v = mval_read(r.output);
-			v = mval_eval(e, v);
-			mval_println(v);
-			mval_del(v);
-
-			mpc_ast_delete(r.output);
-		}
-		else
-		{
-			mpc_err_print(r.error);
-			mpc_err_delete(r.error);
-		}
-
-		free(input);
+		interpret_input(GLOBAL_ENV, input);
 	}
 }
 
@@ -49,22 +69,21 @@ int main(int argc, char* argv[])
 	init_lang_parsers();
 	atexit(cleanup_lang_parsers);
 
+	init_global_env();
+	atexit(delete_global_env);
+
 	puts("Misp Version " VERSION_INFO);
 	puts("Empty input to exit\n");
-
-	menv* e = menv_new();
-	menv_add_builtins(e);
 
 	if (argc >= 2)
 	{
 		for (int i = 1; i < argc; ++i)
 		{
-			load_file(e, argv[i]);
+			load_file(GLOBAL_ENV, argv[i]);
 		}
 
-		menv_del(e);
 		return 0;
 	}
 
-	return interactive_prompt(e);
+	return interactive_prompt();
 }
