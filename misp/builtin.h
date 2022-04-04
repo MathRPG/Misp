c5c9bbe9c59d70775fd21260e4eb2c4376de25b8
Matheus Augusto <matheus-augusto.santos@unesp.br>
2022-04-04 10:30:38 -0300

    extracted some functions to builtin.{c,h}

--- misp/builtin.h
+++ misp/builtin.h
@@ -1,1 +1,34 @@
 #pragma once
+
+#define BUILTIN_CMP(F) \
+    F(gt, >)                  \
+    F(lt, <)                  \
+    F(ge, >=)                 \
+    F(le, <=)
+
+mval* builtin_def(menv* e, mval* a);
+mval* builtin_put(menv* e, mval* a);
+mval* builtin_lambda(menv* e, mval* a);
+mval* builtin_if(menv* e, mval* a);
+mval* builtin_print(__attribute__((unused)) menv* e, mval* a);
+mval* builtin_error(__attribute__((unused)) menv* e, mval* a);
+mval* builtin_load(menv* e, mval* a);
+
+mval* builtin_list(menv* e, mval* a);
+mval* builtin_head(menv* e, mval* a);
+mval* builtin_tail(menv* e, mval* a);
+mval* builtin_eval(menv* e, mval* a);
+mval* builtin_join(menv* e, mval* a);
+
+mval* builtin_add(menv* e, mval* a);
+mval* builtin_sub(menv* e, mval* a);
+mval* builtin_mul(menv* e, mval* a);
+mval* builtin_div(menv* e, mval* a);
+
+#define FUNC_HEADER(name, symb) \
+    mval* builtin_##name(menv* e, mval* a);
+
+BUILTIN_CMP(FUNC_HEADER)
+
+mval* builtin_eq(menv* e, mval* a);
+mval* builtin_ne(menv* e, mval* a);
--- misp/misp.h
+++ misp/misp.h
@@ -56,7 +56,21 @@ struct MispEnv
 
 menv* menv_new(void);
 void menv_add_builtins(menv* e);
+void menv_def(menv* e, mval* k, mval* v);
+void menv_put(menv* e, mval* k, mval* v);
 void menv_del(menv* e);
+
+mval* mval_sexpr(void);
+mval* mval_err(const char* fmt, ...);
+mval* mval_lambda(mval* args, mval* body);
+mval* mval_num(long x);
+
+const char* mtype_name(enum mval_type t);
+
+mval* mval_take(mval* v, int i);
+mval* mval_pop(mval* v, int i);
+mval* mval_join(mval* x, mval* y);
+int mval_eq(mval* x, mval* y);
 
 mval* mval_read(mpc_ast_t* t);
 mval* mval_eval(menv* e, mval* v);
